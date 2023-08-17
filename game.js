f38f1a3d05ee32b26dcbf39a296f247f6b7abf07
Ednilson <ednilsoncastanheira53@gmail.com>
2023-08-17 23:02:10 +0100

    now a random color is selected and the sounds are enabled

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+const gamePattern = [];
+
 // array of colors
 const buttonColors = ["red", "blue", "green", "yellow"];
 
@@ -10,4 +12,25 @@ function nextSequence() {
 
 function randomChosenColor(randomNumber) {
   const chosenColor = buttonColors[randomNumber];
+  gamePattern.push(chosenColor);
+  return chosenColor;
 }
+
+let ranNum = nextSequence();
+
+let chosenColor = randomChosenColor(ranNum);
+
+// let buttonSelector = "div" + "#" + chosenColor + ".btn." + chosenColor;
+// let buttonSelector = "div" + "#" + chosenColor;
+let buttonSelector = "." + chosenColor;
+
+alert(buttonSelector);
+
+// makes the button flash
+$(buttonSelector).toggleClass("flash");
+
+// plays the sound of the selected button
+$(buttonSelector).on("click", function () {
+  const audio = new Audio("./sounds/" + chosenColor + ".mp3");
+  audio.play();
+});
