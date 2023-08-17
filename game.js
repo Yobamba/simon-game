17b710b25b742e4c0936f6805a81f8b9bac0eb68
Ednilson <ednilsoncastanheira53@gmail.com>
2023-08-17 18:54:10 +0100

    created the nextSequence function that generates random numbers

--- game.js
+++ game.js
@@ -1,1 +1,9 @@
 // $("body").css("background-color", "red");
+
+// 1. Inside game.js create a new function called nextSequence()
+function nextSequence() {
+  //2. Inside the new function generate a new random number between 0 and 3,
+  //and store it in a variable called randomNumber
+
+  const randomNumber = Math.floor(Math.random() * 4);
+}
